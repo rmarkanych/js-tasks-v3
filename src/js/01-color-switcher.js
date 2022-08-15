ead7343a82e5d4cb1718fb544d175ea70f4d89c7
Roman <roman.markanych@gmail.com>
2022-08-16 02:13:00 +0300

    add 1-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const changeTime = 1000;
 
+startBtn.addEventListener('click', () => {
+  const idInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+  }, changeTime);
+
+  stopBtn.addEventListener('click', () => {
+    clearInterval(idInterval);
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', '');
+  });
+});
