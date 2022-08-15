e27a01e3e10791031303dd02ba57607a09d58b21
rmarkanych <roman.markanych@gmail.com>
2022-08-15 23:14:04 +0000

    Deploying to gh-pages from @ rmarkanych/goit-js-hw-09@ead7343a82e5d4cb1718fb544d175ea70f4d89c7 🚀

---
+++ 01-color-switcher.039be58a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{const r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",""),e.removeAttribute("disabled")}),1e3);e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","")}))}));
+//# sourceMappingURL=01-color-switcher.039be58a.js.map
